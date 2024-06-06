63ff166c90610e70a9049dc0723c71180f9890f8
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-06-06 11:02:22 +0200

    Transformation working
    All object initializations are transformed to get from "this.value = new A()" to "be.unamur.snail.RegisterUtils.register(this, this.value = new A())"

---
+++ src/main/java/be/unamur/snail/objectinstantiation/FieldInitializationProcessor.java
@@ -0,0 +1,52 @@
+package be.unamur.snail.objectinstantiation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtExecutableReference;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+public class FieldInitializationProcessor extends AbstractProcessor<CtConstructor<?>> {
+    private final Logger log = LoggerFactory.getLogger(FieldInitializationProcessor.class);
+
+    @Override
+    public void process(CtConstructor<?> constructor) {
+        Factory factory = getFactory();
+
+        // Create a reference to "this"
+        CtTypeReference<?> declaringTypeReference = constructor.getDeclaringType().getReference();
+        CtExpression<?> thisReference = factory.createThisAccess(declaringTypeReference);
+
+        // Iterate over all statements in the constructor's body
+        for (CtAssignment<?,?> assignment : constructor.getBody().getElements(new TypeFilter<>(CtAssignment.class))) {
+            CtExpression<?> assignedExpression = assignment.getAssignment();
+            if (assignedExpression instanceof CtConstructorCall<?>) {
+                // Create the assignment for the field
+                CtAssignment<?,?> newAssignment = assignment.clone();
+
+                CtTypeReference<?> registerUtilsType = factory.Type().createReference("be.unamur.snail.RegisterUtils");
+                CtExecutableReference<?> registerMethod = factory.Executable().createReference(
+                        registerUtilsType,
+                        factory.Type().voidPrimitiveType(),
+                        "register",
+                        factory.Type().OBJECT,
+                        factory.Type().OBJECT
+                );
+                CtInvocation<?> registerInvocation = factory.Code().createInvocation(
+                        factory.Code().createTypeAccess(registerUtilsType),
+                        registerMethod,
+                        thisReference,
+                        newAssignment
+                );
+
+                // Replace the original assignment with the register invocation
+                assignment.replace(registerInvocation);
+            }
+        }
+    }
+}
--- src/main/java/be/unamur/snail/objectinstantiation/Main.java
+++ src/main/java/be/unamur/snail/objectinstantiation/Main.java
@@ -3,12 +3,6 @@ package be.unamur.snail.objectinstantiation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spoon.Launcher;
-import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtClass;
-import spoon.reflect.visitor.filter.TypeFilter;
-import spoon.support.compiler.FileSystemFolder;
-
-import java.util.Map;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -22,37 +16,15 @@ public class Main {
         String outputPath = args[1];
 
         Launcher launcher = new Launcher();
-        launcher.addInputResource(new FileSystemFolder(inputPath));
-        launcher.buildModel();
-
-        CtModel model = launcher.getModel();
-        FieldWriterTransformer transformer = new FieldWriterTransformer(launcher.getFactory());
-
-        for (CtClass<?> clazz : model.getElements(new TypeFilter<>(CtClass.class))) {
-            clazz.accept(transformer);
-        }
-
-        // Save the modified model to the output directory
-        log.info("Setting source output directory to {}", outputPath);
+        launcher.addInputResource(inputPath);
         launcher.setSourceOutputDirectory(outputPath);
+
+        launcher.getEnvironment().setNoClasspath(true);
+
+        launcher.addProcessor(new FieldInitializationProcessor());
+        launcher.buildModel();
+        launcher.process();
         launcher.prettyprint();
         log.info("Finished output to directory !");
-
-        /*CtModel model = launcher.getModel();
-        ObjectCreationCounterVisitor visitor = new ObjectCreationCounterVisitor();
-
-        for (CtClass<?> clazz : model.getElements(new TypeFilter<>(CtClass.class))) {
-            for (CtConstructor<?> constructorCall : clazz.getConstructors()) {
-                visitor.visitCtConstructor(constructorCall);
-            }
-        }
-
-        Map<CtConstructor<?>, Integer> constructorDepthMap = visitor.getConstructorDepthMap();
-
-        for (Map.Entry<CtConstructor<?>, Integer> entry : constructorDepthMap.entrySet()) {
-            if (entry.getValue() > 1) {
-                log.info("Constructor: {}, Depth: {}", entry.getKey().getSignature(), entry.getValue());
-            }
-        }*/
     }
 }
