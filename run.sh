273e0f973d4ce143aabbb559c8a9e3f4cc48262f
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-12-02 14:16:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,27 +19,34 @@ output_source_code="${output_repo_path}${src_folder_path}/src/main/java/"
 
 # Remove old output folder
 echo "Removing ${output_repo_path}..."
+echo "rm -rf ${output_repo_path}"
 rm -rf "$output_repo_path"
 
 # Copy the project into a new output folder where the transformed classes will be put
 echo "Copying ${input_repo_path} into ${output_repo_path}"
+echo "cp -r ${input_repo_path} ${output_repo_path}"
 cp -r "$input_repo_path" "$output_repo_path"
 
+echo "mvn clean package"
 mvn clean package
 cd ./target || return
+echo "java -jar object-instantiation-1.0-SNAPSHOT-jar-with-dependencies.jar ${input_source_code} ${output_source_code}"
 java -jar object-instantiation-1.0-SNAPSHOT-jar-with-dependencies.jar "$input_source_code" "$output_source_code"
 
 # Copy be.unamur.snail.register.RegisterUtils to the output folder
 register_path="/home/jerome/Documents/Assistant/Recherche/joular-scripts/object-instantiation/src/main/java/be/unamur/snail/register/."
+echo "mkdir -p ${output_source_code}/be/unamur/snail/register"
 mkdir -p "${output_source_code}/be/unamur/snail/register"
+echo "cp -r ${register_path} ${output_source_code}/be/unamur/snail/register"
 cp -r "$register_path" "${output_source_code}/be/unamur/snail/register/"
 
 # Execute spring-boot tests with the transformation
-cd "$output_repo_path" || return
-pwd
-
-export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
-./gradlew clean spring-boot-project:spring-boot:test
+#cd "$output_repo_path" || return
+#pwd
+#
+#export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
+#echo "./gradlew clean spring-boot-project:spring-boot:test"
+#./gradlew clean spring-boot-project:spring-boot:test
 
 #export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 #mvn clean test
