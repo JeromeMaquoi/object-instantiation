c952831ad8306f81eb49c25c86d8b5494dbfda2a
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-12-12 14:02:45 +0100

    Counting number of attributes for each constructor into a csv

--- src/main/java/be/unamur/snail/ConstructorInstrumentationProcessor.java
+++ src/main/java/be/unamur/snail/ConstructorInstrumentationProcessor.java
@@ -13,22 +13,29 @@ import spoon.reflect.reference.CtExecutableReference;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ConstructorInstrumentationProcessor extends AbstractProcessor<CtConstructor<?>> {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private static final String LOG_FILE_PATH = "assignment_counts.csv";
 
     @Override
     public void process(CtConstructor<?> constructor) {
         //log.info("Constructor : {}", constructor.getSignature());
         if (constructor.getBody() == null) return;
 
+        int assignmentCount = 0;
         Factory factory = getFactory();
 
         for (CtAssignment<?, ?> assignment : constructor.getBody().getElements(new TypeFilter<>(CtAssignment.class))) {
             if (assignment.getAssigned() instanceof CtFieldAccess<?> fieldAccess) {
                 if (fieldAccess.getTarget() instanceof CtThisAccess<?> || fieldAccess.getTarget() == null) {
+                    assignmentCount++;
                     CtTypeReference<?> declaringTypeReference = constructor.getDeclaringType().getReference();
                     CtExpression<?> thisReference = factory.createThisAccess(declaringTypeReference);
                     CtInvocation<?> registerInvocation = createRegisterInvocation(factory, assignment, thisReference);
@@ -37,6 +44,7 @@ public class ConstructorInstrumentationProcessor extends AbstractProcessor<CtCon
                 }
             }
         }
+        writeCountToFile(constructor.getSignature(), assignmentCount);
     }
 
     private CtInvocation<?> createRegisterInvocation(Factory factory, CtAssignment<?, ?> assignment, CtExpression<?> thisReference) {
@@ -55,5 +63,20 @@ public class ConstructorInstrumentationProcessor extends AbstractProcessor<CtCon
                 newAssignment
         );
         return registerInvocation;
+    }
+
+    private void writeCountToFile(String constructorSignature, int count) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_PATH, true))) {
+            log.info("Writing {} to CSV file", constructorSignature);
+            File csvFile = new File(LOG_FILE_PATH);
+            if (csvFile.length() == 0) {
+                writer.write("Constructor,Assignment Count");
+                writer.newLine();
+            }
+            writer.write(String.format("\"%s\",%d", constructorSignature, count));
+            writer.newLine();
+        } catch (IOException e) {
+            log.error("Failed to write counts to CSV: {}", e.getMessage());
+        }
     }
 }
